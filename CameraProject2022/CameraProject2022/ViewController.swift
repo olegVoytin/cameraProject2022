f194dd73ce77b5bd3b2c6a1cfbc934f881b15984
Oleg Voytin <ovoytin@sberhealth.ru>
2022-03-16 18:29:03 +0500

    initial

---
+++ CameraProject2022/CameraProject2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraProject2022
+//
+//  Created by Олег Войтин on 16.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
